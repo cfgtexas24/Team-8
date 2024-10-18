dd2db8a8066fcd8dad55959b4881cdcadee8c05b
Andy Ouyang <andyouyang@MacBook-Pro.local>
2024-10-18 18:58:07 -0500

    Resolved conflicts and completed changes for backend and rebirther

--- rebirther/src/pages/UserJob.tsx
+++ rebirther/src/pages/UserJob.tsx
@@ -1,25 +1,25 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import './UserJob.css';
 
-const Tab1: React.FC = () => {
+const UserJob: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>UserJob</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">UserJob</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <ExploreContainer name="UserJob page" />
       </IonContent>
     </IonPage>
   );
 };
 
-export default Tab1;
+export default UserJob;
