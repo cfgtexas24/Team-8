5a47eeab7c35a4fa374cc64eb8ce490af55c74ad
jailingineni <jailingineni@gmail.com>
2024-10-18 19:12:06 -0500

    Created the Ion Card
    Created a Jobs.Json File add User.TSX and UserDetail.css

--- rebirther/src/pages/Tab1.tsx
+++ rebirther/src/pages/Tab1.tsx
@@ -1,13 +1,34 @@
+<<<<<<< Updated upstream
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonCard } from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+<<<<<<<< Updated upstream:rebirther/src/pages/Tab1.tsx
+import './Tab1.css';
+========
+import './UserJob.css';
+import jobsData from '../jobs.json'
+
+>>>>>>>> Stashed changes:rebirther/src/pages/UserJob.tsx
+=======
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
+import React from 'react';
+>>>>>>> Stashed changes
 
 const Tab1: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
+<<<<<<< Updated upstream
+<<<<<<<< Updated upstream:rebirther/src/pages/Tab1.tsx
           <IonTitle>Tab 1</IonTitle>
+========
+          <IonTitle>User Job</IonTitle>
+>>>>>>>> Stashed changes:rebirther/src/pages/UserJob.tsx
+=======
+          <IonTitle>Tab 1</IonTitle>
+>>>>>>> Stashed changes
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
@@ -16,10 +37,33 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
+<<<<<<< Updated upstream
+<<<<<<<< Updated upstream:rebirther/src/pages/Tab1.tsx
         <ExploreContainer name="Tab 1 page" />
+========
+        
+        {/* Mapping over the jobs array to display buttons for each job */}
+        {jobsData.jobs.map((job, index) => (
+          <IonCard className="custom-card" key={index}>
+            {job.title} at {job.company}
+          </IonCard>
+        ))}
+
+>>>>>>>> Stashed changes:rebirther/src/pages/UserJob.tsx
+=======
+>>>>>>> Stashed changes
       </IonContent>
     </IonPage>
   );
 };
+<<<<<<< Updated upstream
+<<<<<<<< Updated upstream:rebirther/src/pages/Tab1.tsx
 
 export default Tab1;
+========
+export default UserJob;
+>>>>>>>> Stashed changes:rebirther/src/pages/UserJob.tsx
+=======
+
+export default Tab1;
+>>>>>>> Stashed changes
