b5a10484a71e219ecffb7d4c5d969971280314e6
ezhu0806 <emilyzhu86@gmail.com>
2024-10-18 22:20:18 -0500

    add & changed inputs, edited style

--- rebirther/src/pages/Profile.tsx
+++ rebirther/src/pages/Profile.tsx
@@ -1,8 +1,30 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonInput, IonItem, IonCard, IonText } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonInput, IonItem, IonCard, IonText, IonLabel } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Profile.css';
+import React, {useState} from 'react';
 
 const Profile: React.FC = () => {
+  //populate the skills user inputted
+  const[newSkill, setNewSkill] = useState<string>('');
+  const[skills, setSkills] = useState<string[]>([]);
+
+  //populate the experiences user inputted
+  const[newExp, setNewExp] = useState<string>('');
+  const[exp, setExp] = useState<string[]>([]);
+
+  const addSkill = () =>{
+    if (newSkill.trim() !== ''){
+      setSkills([...skills, newSkill]); //add new skill to list
+      setNewSkill(''); //clear input field
+    }
+  };
+
+  const addExp = () =>{
+    if (newExp.trim() !== ''){
+      setExp([...exp, newExp]); //add new skill to list
+      setNewExp(''); //clear input field
+    }
+  };
   return (
     <IonPage>
       <IonHeader>
@@ -10,29 +32,51 @@ const Profile: React.FC = () => {
           <IonTitle>Profile</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonCard className="First">
-        <IonInput>First Name:</IonInput>
+      <IonContent fullscreen className="profile">
+        <IonCard className="first">
+          <IonLabel className="custom-font">First Name:</IonLabel>
+          <IonInput type="text"></IonInput>
         </IonCard>
-        <IonCard className="Last">
-        <IonInput>Last Name:</IonInput>
+        <IonCard className="last">
+        <IonLabel className="custom-font">Last Name:</IonLabel>
+          <IonInput type="text"></IonInput>
         </IonCard>
-        <IonCard className="Skills">
-        <IonInput>Add Your Skills:</IonInput>
+        <IonCard className="age">
+          <IonLabel className="custom-font">Age:</IonLabel>
+          <IonInput type="number"></IonInput>
         </IonCard>
-        <IonCard className="Age">
-        <IonInput>Age:</IonInput>
+        <IonCard className="phone-number">
+          <IonLabel className="custom-font">Phone Number:</IonLabel>
+        <IonInput type="tel"></IonInput>
+          </IonCard>
+        <IonCard className="skills">
+          <IonItem>
+            <IonLabel className="custom-font" position="floating">Enter Skills:</IonLabel>
+            <IonInput className="spacing"
+              value={newSkill}
+              placeholder="Ex: Java"
+              onIonChange={e => setNewSkill(e.detail.value!)} clearInput 
+              />
+          </IonItem>
+          <IonButton onClick={addSkill} disabled={!newSkill.trim()}>Add New Skill</IonButton>
         </IonCard>
         <IonCard className="Experience">
-        <IonInput>Add your Current Experiences:</IonInput>
+          <IonItem>
+            <IonLabel position="floating">Enter your past and current experiences</IonLabel>
+            <IonInput className="spacing" value={newExp}
+            placeholder="Ex..."
+            onIonChange={(e) => setNewExp(e.detail.value!)} clearInput/>
+          </IonItem>
+          <IonButton onClick={addExp} disabled={!newExp.trim()}>Add New Experience</IonButton>
         </IonCard>
         <IonCard>
           <IonItem>
-            <IonText>Please Upload Resume</IonText>
-            <input type="file"></input>
+          <IonLabel className="custom-font">Please Upload Resume:</IonLabel>
+            <IonText></IonText>
+            <input className="custom-font" type="file"></input>
           </IonItem>
         </IonCard>
-        <IonButton>Save Information</IonButton>
+        <IonButton className="button">Save Information</IonButton>
       </IonContent>
     </IonPage>
   );
